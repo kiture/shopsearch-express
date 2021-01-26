b60e44c9f6b3da98e47f98ad6c68e7a01dcacaa6
MBorysewicz <mateusz.borysewicz@logicims.com>
2021-01-26 17:26:28 +0100

    test

--- server2.js
+++ server2.js
@@ -16,7 +16,11 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
+app.use('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+});
 
 module.exports = app;
 module.exports.handler = serverless(app);
