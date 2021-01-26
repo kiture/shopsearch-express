e1b1b9eceecb3d31c9f02990aba58e61b433fd7b
MBorysewicz <mateusz.borysewicz@logicims.com>
2021-01-26 18:29:23 +0100

    test

--- express/server.js
+++ express/server.js
@@ -4,6 +4,24 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const jsdom = require("jsdom");
+const fs = require('fs');
+const {JSDOM} = jsdom;
+const fetch = require("node-fetch");
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+let tokenType;
+let accessToken;
+
+const dbAllegro = low(new FileSync('allegroCategories.json'));
+const dbOlx = low(new FileSync('olxCategories.json'));
+
+const ALLEGRO_CLIENT_ID = "aea655fde4f04b349a4bbad8102296a3";
+const ALLEGRO_CLIENT_SECRET = "MVe8KDA305fHqWZrtzY9Ce3maTJ2bDTVdmfTEbnNAThLtFUsF1VsU6YELDYuVfLE";
+const ALLEGRO_API_URL = "https://api.allegro.pl";
+const OLX_URL = "https://www.olx.pl";
 
 const router = express.Router();
 router.get('/', (req, res) => {
@@ -13,10 +31,17 @@ router.get('/', (req, res) => {
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.get('/allegro/categories', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(dbAllegro.read().value()));
+});
 
-app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use(cors());
+app.use(express.static('client'));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({limit: '50mb', extended: true}));
 
 module.exports = app;
 module.exports.handler = serverless(app);
